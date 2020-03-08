d515c68f5db008e13e2dd535aaf4d8475072d7bc
Andrew Ruthven <puck@catalystcloud.nz>
2020-03-08 09:38:27 +0000

    Switch to using PagerDuty::Agent

--- lib/RT/Action/NotifyPagerDuty.pm
+++ lib/RT/Action/NotifyPagerDuty.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use LWP::UserAgent;
 use JSON;
+use PagerDuty::Agent;
 
 package RT::Action::NotifyPagerDuty;
 use base qw(RT::Action);
@@ -178,30 +179,13 @@ sub Commit {
     # If $pd_action isn't set, then we have nothing to do.
     return 1 unless defined $pd_action;
 
-    my $ticket = $self->TicketObj;
-    my $queue  = $ticket->QueueObj;
-
-    my $queue_priority_cf_name = RT->Config->Get('PagerDutyQueueCFPriority') || 'Incident Priority';
-    my $queue_priority = $queue->FirstCustomFieldValue($queue_priority_cf_name);
-
-    # Set the priority to what PagerDuty supports.
-    if ($queue_priority) {
-        $queue_priority = lc($queue_priority);
-        if ($queue_priority !~ /^critical|warning|error|info$/) {
-            $RT::Logger->error("Pager Duty priority is $queue_priority, which isn't supported by PD, changing to critical");
-            $queue_priority = 'critical';
-        }
+    my ($result, $error) = $self->_pagerduty_submit($pd_action);
+    if (! defined $result) {
+        $RT::Logger->error("Failed to raise incident on Pager Duty, error: $error");
+        return 1;
     }
 
-    my $queue_service_cf_name = RT->Config->Get('PagerDutyQueueCFService') || 'Incident Service';
-    my $queue_service = $queue->FirstCustomFieldValue($queue_service_cf_name);
-
-    my $txn_content = $self->_PDEnqueue(
-        queue_service => $queue_service,
-        pd_action     => $pd_action,
-    );
-
-    return unless defined $txn_content;
+    $RT::Logger->info("Successfully raised incident on Pager Duty, dedup_key: $result");
 
     # We need to give RT::Record::_NewTransaction a MIME object to have it
     # store our content for us.
@@ -220,181 +204,60 @@ sub Commit {
     return 1;
 }
 
-# Use the Events v2 API: https://v2.developer.pagerduty.com/docs/events-api-v2
-sub _PDEnqueue {
-    my $self = shift;
-    my %args = (
-        queue_service  => undef,
-        queue_priority => undef,
-        pd_action      => 'trigger',
-        @_
-    );
-
+sub _pagerduty_submit {
+    my ($self, $action) = @_;
     my $routing_key = RT->Config->Get('PagerDutyRoutingKey');
 
-    my $payload = {
-        routing_key  => $routing_key,
-        event_action => $args{pd_action},
-        dedup_key    => 'rt#' . $self->TicketObj->id,
-        payload => {
-            summary  => $self->TicketObj->Subject,
-            source   => $args{queue_service}  || 'RT',
-            severity => $args{queue_priority} || 'critical',
-            class    => 'Ticket',
-        },
+    my $agent     = PagerDuty::Agent->new( routing_key => $routing_key );
+    my $dedup_key = 'rt#' . $self->TicketObj->id;
+
+    if    ($action eq 'acknowledge') {
+        return ($agent->acknowledge_event(dedup_key => $dedup_key, summary => 'Ticket in RT has an owner'), $@);
+
+    elsif ($action eq 'resolve') {
+        return ($agent->resolve_event($dedup_key), $@);
+
+    elsif ($action eq 'trigger') {
+        return ($self->trigger_event($dedup_key, $agent), $@);
+    }
+}
+
+sub _trigger_event {
+    my ($self, $dedup_key, $agent) = @_;
+
+    my $ticket = $self->TicketObj;
+    my $queue  = $ticket->QueueObj;
+
+    my $queue_priority_cf_name = RT->Config->Get('PagerDutyQueueCFPriority') || 'Incident Priority';
+    my $queue_priority = $queue->FirstCustomFieldValue($queue_priority_cf_name);
+
+    # Set the priority to what PagerDuty supports.
+    if ($queue_priority) {
+        $queue_priority = lc($queue_priority);
+        if ($queue_priority !~ /^critical|warning|error|info$/) {
+            $RT::Logger->error("Pager Duty priority is $queue_priority, which isn't supported by PD, changing to critical");
+            $queue_priority = 'critical';
+        }
+    }
+
+    my $queue_service_cf_name = RT->Config->Get('PagerDutyQueueCFService') || 'Incident Service';
+    my $queue_service = $queue->FirstCustomFieldValue($queue_service_cf_name);
+
+    my $result = $agent->trigger_event(
+        dedup_key => $dedup_key,
+        summary   => $self->TicketObj->Subject,
+        source    => $args{queue_service}  || 'RT',
+        severity  => $args{queue_priority} || 'critical',
+        class     => 'Ticket',
         links => [
             {
                 href => RT->Config->Get('WebBaseURL') . '/' . $self->TicketObj->id,
                 text => 'RT Ticket',
             },
         ],
-    };
-
-    my $payload_json = JSON::encode_json($payload);
-
-    my $ua = LWP::UserAgent->new();
-    $ua->timeout(10);
-
-    $RT::Logger->info('Queuing incident on PagerDuty');
-
-    my $endpoint = "https://events.pagerduty.com/v2/enqueue";
-    my $post_response = $ua->post($endpoint,
-        'Accept'        => 'application/vnd.pagerduty+json;version=2',
-        'Content-Type'  => 'application/json',
-        'Content'       => $payload_json,
     );
 
-    my $txn_content;
-    if ($post_response->is_success) {
-        my $response = JSON::decode_json($post_response->decoded_content);
-
-        $txn_content = 'Response from PagerDuty: ' . $response->{'message'} . "\nStatus from PagerDuty: " . $response->{'status'};
-    } else {
-        $RT::Logger->error('Failed to create incident on PagerDuty (',
-            $post_response->code ,': ', $post_response->message, ', json: ', $post_response->decoded_content, ')');
-
-        $txn_content = 'Failed to create incident in PagerDuty: ' . $post_response->message . "\n" . $post_response->decoded_content;
-
-        if ($post_response->code != 400) {
-            # Queue the request up for a retransmit.
-        }
-    }
-
-    return $txn_content;
-}
-
-# Use the Incident API: https://api-reference.pagerduty.com/#!/Incidents/get_incidents
-sub _PDCreationAPI {
-    my $self = shift;
-    my %args = (
-        queue          => undef,
-        queue_service  => undef,
-        queue_priority => undef,
-        @_
-    );
-
-    my $queue_from_cf_name = RT->Config->Get('PagerDutyQueueCFFrom') || 'Incident From';
-    my $queue_from = $args{queue}->FirstCustomFieldValue($queue_from_cf_name);
-
-    if (! defined $queue_from) {
-        # Try looking for a global config.
-        $queue_from = RT->Config->Get('PagerDutyFrom');
-    }
-
-    if (! defined $queue_from) {
-        $RT::Logger->error('Failed to create incident on PagerDuty (No From account set in config or queue)');
-        return undef;
-    }
-
-    my $payload = {
-        incident => {
-            type    => 'incident',
-            title   => $self->TicketObj->Subject,
-            from    => $queue_from,
-            service => {
-                id   => $args{queue_service},
-                type => 'service_reference',
-            },
-            incident_key => 'rt#' . $self->TicketObj->id,
-            body    => {
-                type    => 'incident_body',
-                details => RT->Config->Get('WebBaseURL') . '/' . $self->TicketObj->id,
-            },
-        },
-    };
-
-    if (defined $args{queue_priority}) {
-        $payload->{'priority'} = {
-            id => $args{queue_priority},
-            type => "priority_reference",
-        };
-    }
-
-# How do you add a link via this API?
-#        links => [
-#            {
-#                href => RT->Config->Get('WebBaseURL') . '/' . $self->TicketObj->id,
-#                text => 'RT Ticket',
-#            },
-#        ],
-
-    my $payload_json = JSON::encode_json($payload);
-
-    my $ua = LWP::UserAgent->new();
-    $ua->timeout(10);
-
-    $RT::Logger->info('Creating incident on PagerDuty');
-
-    my $api_token = RT->Config->Get('PagerDutyAPIToken');
-
-    my $endpoint = "https://api.pagerduty.com/incidents";
-    my $post_response = $ua->post($endpoint,
-        'Accept'        => 'application/vnd.pagerduty+json;version=2',
-        'Authorization' => "Token token=$api_token",
-        'Content-Type'  => 'application/json',
-        'Content'       => $payload_json,
-    );
-
-    my $txn_content;
-    if ($post_response->is_success) {
-        my $response = JSON::decode_json($post_response->decoded_content);
-
-        $txn_content = join("\n",
-          'Status from PagerDuty: ' . $response->{'status'},
-          'PagerDuty link: ' . $response->{'html_url'},
-          'Assigned in PagerDuty to: ' . $response->{'assignments'}{'assignee'}{'summary'}
-        );
-
-        my $ticket_pd_id_cf_name = RT->Config->Get('PagerDutyTicketCFID') || 'Incident Id';
-        my ($cf_status, $cf_msg) = $self->TicketObj->AddCustomFieldValue(
-            Field => $ticket_pd_id_cf_name,
-            Value => $response->{'id'},
-            RecordTransaction => 0,
-        );
-        if ($cf_status == 0) {
-            $txn_content .= "\nFailed to set CF for PagerDuty Id: $cf_msg";
-        }
-
-        my $ticket_pd_link_cf_name = RT->Config->Get('PagerDutyTicketCFLink') || 'Incident Link';
-        ($cf_status, $cf_msg) = $self->TicketObj->AddCustomFieldValue(
-            Field => $ticket_pd_link_cf_name,
-            Value => $response->{'html_ref'},
-            RecordTransaction => 0,
-        );
-        if ($cf_status == 0) {
-            $txn_content .= "\nFailed to set CF for PagerDuty Link: $cf_msg";
-        }
-    } else {
-        $RT::Logger->error('Failed to create incident on PagerDuty (',
-            $post_response->code ,': ', $post_response->message, ', json: ', $post_response->decoded_content, ')');
-
-        $txn_content = 'Failed to create incident in PagerDuty: ' . $post_response->message . "\n" . $post_response->decoded_content;
-
-        # TODO: Cache to disk for later attempt.
-    }
-
-
-    return $txn_content;
+    return ($result, $@);
 }
 
 1;
